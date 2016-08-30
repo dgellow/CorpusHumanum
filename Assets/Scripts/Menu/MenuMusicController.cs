f6a43094ae360507c63b0c5ee97236d8064a2d2e
dgellow <samuel.elborai@gmail.com>
2016-08-30 10:16:57 +0700

    add music

---
+++ Assets/Scripts/Menu/MenuMusicController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusicController : MonoBehaviour {
+
+	public AudioSource source;
+	public AudioClip start;
+	public AudioClip loop;
+
+	// Use this for initialization
+	void Awake () {
+		StartCoroutine (Play ());
+	}
+
+	IEnumerator Play() {
+		source.clip = start;
+		source.Play ();
+		yield return new WaitForSecondsRealtime (start.length);
+		source.clip = loop;
+		source.loop = true;
+		source.Play ();
+	}
+}
